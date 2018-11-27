3a3368ad3d66cf1d92557f68ceeb4ac7a6445c54
CamilleBlais <45370487+CamilleBlais@users.noreply.github.com>
2018-11-27 11:31:07 -0500

    Update mySketch.js

--- mySketch.js
+++ mySketch.js
@@ -8,8 +8,15 @@ function draw() {
 	strokeWeight(2);
 	stroke(0);
 	fill(55);
-	rect(0,1100,2500,300);
+	rect(0,1100,2800,300);
 	fill(35);
-	rect(0,1150,2500,200);
+	rect(0,1150,2800,200);
+	
+	///////////// maisons
+	
+	fill(100);
+	rect(0,800,200,200);
+	fill(105);
+	rect(200,650,250,350);
 	
 }
