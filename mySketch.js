9fcd075139ccfd3f0a940b2032d17d05d10a33ed
CamilleBlais <45370487+CamilleBlais@users.noreply.github.com>
2018-11-26 23:12:29 -0500

    Update mySketch.js

--- mySketch.js
+++ mySketch.js
@@ -1,11 +1,25 @@
-function setup() {
-	createCanvas(windowWidth, windowHeight);
-	background(125,35,135);
+// Daniel Shiffman
+// http://codingtra.in
+// http://patreon.com/codingtrain
+// Code for: https://youtu.be/KkyIDI6rQJI
 
+// Purple Rain
+// (138, 43, 226)
+// (230, 230, 250) // background
+
+Drop[] drops = new Drop[500];
+
+void setup() {
+  size(640, 360);
+  for (int i = 0; i < drops.length; i++) {
+    drops[i] = new Drop();
+  }
 }
 
-function draw() {
-	
-fill(80);
-rect(0,1200,windowWitdh,800);
+void draw() {
+  background(230, 230, 250);
+  for (int i = 0; i < drops.length; i++) {
+    drops[i].fall();
+    drops[i].show();
+  }
 }
