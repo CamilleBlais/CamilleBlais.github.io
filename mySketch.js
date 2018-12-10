60609d17cb8c68134edf3e417e9053b503cb0dfd
CamilleBlais <45370487+CamilleBlais@users.noreply.github.com>
2018-12-10 16:45:58 -0500

    Update mySketch.js

--- mySketch.js
+++ mySketch.js
@@ -19,7 +19,7 @@ var getJSON = function(url, callback) {
 };
 
 function setup(){
-  createCanvas(2850, 1600);
+  createCanvas(windowWidth, windowHeight);
   background(255,255,235);
   getJSON('https://api.openweathermap.org/data/2.5/weather?q=Montreal,ca&APPID=c24766a030fea56b0f4b66352b1ecff0',
   function(err, data) {
