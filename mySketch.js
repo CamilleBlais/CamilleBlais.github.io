f46a75da5752eebc558b8e90324a993fa834b748
CamilleBlais <45370487+CamilleBlais@users.noreply.github.com>
2018-12-11 11:56:18 -0500

    Update mySketch.js

--- mySketch.js
+++ mySketch.js
@@ -3,13 +3,15 @@ var BriqueY = 0;
 var BriqueX2 = 0;
 var BriqueY2 = 0;
 
+var date = {year: 1986, month: 1, day: 1, hours: 1, minutes: 52, seconds: 0};
+
 $const.tlong = -71.10; // longitude
-  $const.glat = 42.37; // latitude
-  $processor.init ();
-  // sun, mercury, venus, moon, mars, jupiter, saturn, uranus, neptune, pluto, chiron, sirius
-  var body = $moshier.body.moon;
-  $processor.calc (date, body);
-  console.log(body.position);
+$const.glat = 42.37; // latitude
+$processor.init ();
+// sun, mercury, venus, moon, mars, jupiter, saturn, uranus, neptune, pluto, chiron, sirius
+var body = $moshier.body.moon;
+$processor.calc (date, body);
+console.log(body.position);
 
 var getJSON = function(url, callback) {
     var xhr = new XMLHttpRequest();
