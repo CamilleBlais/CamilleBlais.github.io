9ae5b963f00e78d91d7d08bf7f0656506ff2d3d9
CamilleBlais <45370487+CamilleBlais@users.noreply.github.com>
2018-12-12 00:18:21 -0500

    Update mySketch.js

--- mySketch.js
+++ mySketch.js
@@ -3,7 +3,7 @@ var BriqueY = 0;
 var BriqueX2 = 0;
 var BriqueY2 = 0;
 
-//var date = {year: 1986, month: 1, day: 1, hours: 1, minutes: 52, seconds: 0};
+var date = {year: 1986, month: 1, day: 1, hours: 1, minutes: 52, seconds: 0};
 
 $const.tlong = -71.10; // longitude
 $const.glat = 42.37; // latitude
