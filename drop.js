028b7ede315632157b25246ed0032a93837aae0c
CamilleBlais <45370487+CamilleBlais@users.noreply.github.com>
2018-12-13 20:39:01 -0500

    Update drop.js

--- drop.js
+++ drop.js
@@ -6,25 +6,25 @@
 
 function Drop() {
   this.x = random(width);
-  this.y = random(-600, 0);
+  this.y = random(-500, -50);
   this.z = random(0, 20);
   this.len = map(this.z, 15, 20, 15, 20);
   this.yspeed = map(this.z, 15, 20, 15, 20);
 
   this.fall = function() {
     this.y = this.y + this.yspeed;
-    var grav = map(this.z, 15, 20, 0, 0.3);
+    var grav = map(this.z, 15, 20, 0.2, 0.3);
     this.yspeed = this.yspeed + grav;
 
     if (this.y > height) {
       this.y = random(-200, -100);
-      this.yspeed = map(this.z, 15, 20, 4, 10);
+      this.yspeed = map(this.z, 15, 20, 15, 20);
     }
   }
 
   this.show = function() {
-    var thick = map(this.z, 15, 20, 1, 3);
-    strokeWeight(thick);
+   
+    strokeWeight(3);
     stroke(131, 192, 247);
     line(this.x, this.y, this.x, this.y+this.len);
   }
