8608c134037832c79aa053eb6775a9cb470dc8e4
CamilleBlais <45370487+CamilleBlais@users.noreply.github.com>
2018-12-16 03:37:07 -0500

    Update mySketch.js

--- mySketch.js
+++ mySketch.js
@@ -78,7 +78,7 @@ function setup(){
 	    meteo = data.weather[0].description;
       
       console.log(meteo);
-	    meteo = "moderate rain";
+	    meteo = moderate rain;
     }
   }); 
 	if(meteo == "light rain"){
