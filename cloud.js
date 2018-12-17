52afd600d349af3685268464596eb751103ac4b2
CamilleBlais <45370487+CamilleBlais@users.noreply.github.com>
2018-12-17 04:06:52 -0500

    Update cloud.js

--- cloud.js
+++ cloud.js
@@ -21,7 +21,7 @@ function Cloud() {
    	noStroke();
 	  fill(250,250,255,200);
 	  rect(this.x,this.y,this.len,this.high,this.high/2);
-	  rect(this.x,this.y-this.high/3,this.len/2,this.high,this.high/2);
+	  rect(this.x+this.len/8,this.y-this.high/2,this.len/2,this.high,this.high/2);
 	  rect(this.x+this.len/2,this.y-this.high/3,this.len/2,this.high,this.high/2);
     }
 
