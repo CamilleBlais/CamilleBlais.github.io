6d21bc81a540b22619029d5bdf76b20eb58e8b76
CamilleBlais <45370487+CamilleBlais@users.noreply.github.com>
2018-12-16 20:54:34 -0500

    Update star.js

--- star.js
+++ star.js
@@ -1,12 +1,5 @@
 function Star() {
-  this.x = random(width);
-  this.y = random(-20, 500);
- 
-
-
-  this.show = function() {
-   
     stroke(230, 230, 250);
-    point(this.x, this.y);
+    point(random(width), random(-20, 500));
   }
-}
+
