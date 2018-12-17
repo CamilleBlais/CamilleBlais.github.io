5914561716e618df33a2f8f237e5f49347eaf49e
CamilleBlais <45370487+CamilleBlais@users.noreply.github.com>
2018-12-17 00:50:08 -0500

    Update snowflake2.js

--- snowflake2.js
+++ snowflake2.js
@@ -7,21 +7,30 @@ var y = random(-1000,50);
 this.pos = createVector(x,y);
 this.vel = createVector(0,0);
 this.acc = createVector();
-this.r = random(4,8);
+this.r = random(3,13);
+//this.terminalV = this.r;
 }
 
 applyForce(force){
-this.acc.add(force);
+  // parallele effect hack
+ 
+  var f = force.copy();
+  f.mult(this.r); 
+  
+  //var f = force.copy();
+  //f.div(this.mass);
+  this.acc.add(f);
 }
 
 update(){
 this.vel.add(this.acc);
+this.vel.limit(this.r * 0.2);
 this.pos.add(this.vel);
 this.acc.mult(0);
 }
 
 render(){
-stroke(200,0,255);
+stroke(255);
 strokeWeight(this.r);
 point(this.pos.x, this.pos.y);
 }
