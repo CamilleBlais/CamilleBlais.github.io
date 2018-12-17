82c0dff1daff7c6582ad89e25cc02849429a087f
CamilleBlais <45370487+CamilleBlais@users.noreply.github.com>
2018-12-17 04:37:43 -0500

    Create cloud2.js

---
+++ cloud2.js
@@ -0,0 +1,27 @@
+function Cloud2() {
+  this.x = random(0,2900);
+  this.y = random(-50, 350);
+  this.len = random(90, 240);
+  this.high = random(50,90);
+  this.xspeed = random(0.02,0.05);
+  
+  
+    this.move = function() {
+    this.x = this.x + this.xspeed;
+    
+
+    if (this.x > width) {
+      this.x = random(0,2900);
+      this.xspeed = random(0.02,0.05);
+    }
+  }
+    this.show = function() {
+   
+   	noStroke();
+	  fill(250,250,255,200);
+	  rect(this.x,this.y,this.len,this.high,this.high/2);
+	  rect(this.x+this.len/12,this.y-this.high/2.5,this.len/2,this.high,this.high/2);
+	  rect(this.x+this.len/2,this.y-this.high/3,this.len/2,this.high,this.high/2);
+    }
+
+  }
