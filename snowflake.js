23c3e3a3674752783970bcff2e0db1ddd3761f9f
CamilleBlais <45370487+CamilleBlais@users.noreply.github.com>
2018-12-17 00:13:29 -0500

    Create snowflake.js

---
+++ snowflake.js
@@ -0,0 +1,34 @@
+class Snowflake{
+
+constructor(){
+
+var x = random(width);
+var y = random(-25,1600);
+this.pos = createVector(x,y);
+this.vel = createVector(0,0);
+this.acc = createVector();
+this.r = random(4,8);
+}
+
+applyForce(force){
+this.acc.add(force);
+}
+
+update(){
+this.vel.add(this.acc);
+this.pos.add(this.vel);
+this.accmult(0);
+}
+
+render(){
+stroke(255);
+strokeWeight(this.r);
+point(this.pos.x, this.pos.y);
+}
+
+offScreen(){
+return(this.pos.y > height+ this.r);
+}
+
+
+}
