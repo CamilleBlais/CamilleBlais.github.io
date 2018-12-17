8dba9cf51b7cd98ed02a661f560ff56dae5a97ba
CamilleBlais <45370487+CamilleBlais@users.noreply.github.com>
2018-12-17 04:12:00 -0500

    Update cloud.js

--- cloud.js
+++ cloud.js
@@ -1,7 +1,7 @@
 
 function Cloud() {
   this.x = random(0,2900);
-  this.y = random(-50, 400);
+  this.y = random(-50, 350);
   this.len = random(90, 240);
   this.high = random(50,90);
   this.xspeed = random(0.02,0.05);
