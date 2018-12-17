fec5dc13d5dc3e79b140bbced61aa8d5abd8a01e
CamilleBlais <45370487+CamilleBlais@users.noreply.github.com>
2018-12-17 02:11:03 -0500

    Update mySketch.js

--- mySketch.js
+++ mySketch.js
@@ -541,9 +541,9 @@ function draw() {
 	}
 	
 	
-	/if(Realtime > TimeSunrise-1800 && Realtime <= TimeSunrise){ 
-	alphasoleil = map(Realtime,TimeSunrise-1800,TimeSunrise,5,120);
-	}/
+	//if(Realtime > TimeSunrise-1800 && Realtime <= TimeSunrise){ 
+	//alphasoleil = map(Realtime,TimeSunrise-1800,TimeSunrise,5,120);
+	//}
 	if(Realtime > TimeSunrise && Realtime <= TimeSunrise+1000){
 	alphasoleil = map(Realtime,TimeSunrise, TimeSunrise+1000,120,5);
 	}
